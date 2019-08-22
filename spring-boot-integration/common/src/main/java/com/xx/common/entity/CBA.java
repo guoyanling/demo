9bef18f00f017155bc677814520c6108053e9a57
Kealina <kealina@qq.com>
2019-08-23 00:26:58 +0800

    feat: springboot multi maven

---
+++ spring-boot-integration/common/src/main/java/com/xx/common/entity/CBA.java
@@ -0,0 +1,24 @@
+package com.xx.common.entity;
+
+
+public class CBA {
+
+    private int aaa;
+    private int bbb;
+
+    public int getAaa() {
+        return aaa;
+    }
+
+    public void setAaa(int aaa) {
+        this.aaa = aaa;
+    }
+
+    public int getBbb() {
+        return bbb;
+    }
+
+    public void setBbb(int bbb) {
+        this.bbb = bbb;
+    }
+}
