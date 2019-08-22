9f35952ef2db9c1766982641fab5fe8d81da3c47
Kealina <kealina@qq.com>
2019-08-23 00:19:30 +0800

    perf: remove project file

--- springboot-redission/src/test/java/com/example/demo/DemoApplicationTests.java
+++ springboot-redission/src/test/java/com/example/demo/DemoApplicationTests.java
@@ -32,39 +32,47 @@ public class DemoApplicationTests {
 
     public void init() {
         //设置一个key，aaa商品的库存数量为100
-        redisTemplate.opsForValue().set("aaa","100");
-        Assert.assertEquals("100", redisTemplate.opsForValue().get("aaa"));
+        redisTemplate.opsForValue().set("aaa", "5");
+        Assert.assertEquals("5", redisTemplate.opsForValue().get("aaa"));
     }
 
     @Test
-    public void start(){
+    public void start() {
         init();
-        for (int i = 0; i < 2; i++) {
-            new Thread(
-                    () -> {
-                        System.out.println(System.currentTimeMillis()+":Hello world!I'm "+Thread.currentThread().getId());
-                        this.sub();
-                        System.out.println("-------------------------------");
-                    }
-            ).start();
-        }
+        sub();
 
     }
 
-    public void sub () {
+    public void sub() {
         String lockKey = "good";
         //执行的业务代码
-        for(int i=0; i < 55; i++){
-            RLock lock = redisson.getLock(lockKey);
-            lock.lock(60, TimeUnit.SECONDS); //设置60秒自动释放锁  （默认是30秒自动过期）
-            int stock = Integer.parseInt(redisTemplate.opsForValue().get("aaa").toString());
-            if(stock > 0){
-                redisTemplate.opsForValue().set("aaa",(stock-1)+"");
-                System.out.println("test2_:lockkey:"+lockKey+",stock:"+(stock-1)+"");
-            } else {
-                System.out.println("没库存了，stock:"+stock);
-            }
-            lock.unlock(); //释放锁
+        for (int i = 0; i < 10; i++) {
+            new Thread(
+                    () -> {
+                        RLock lock = redisson.getLock(lockKey);
+//                        lock.lock(120, TimeUnit.SECONDS); //设置60秒自动释放锁  （默认是30秒自动过期）
+                        boolean res = false;
+                        try {
+                            res = lock.tryLock(100, 10, TimeUnit.SECONDS);
+                            if (res) {
+                                try {
+                                    int stock = Integer.parseInt(redisTemplate.opsForValue().get("aaa").toString());
+                                    if (stock > 0) {
+                                        redisTemplate.opsForValue().set("aaa", (stock - 1) + "");
+                                        System.out.println("Thread:" + Thread.currentThread().getId() + ":test2_:lockkey:" + lockKey + ",stock:" + (stock - 1) + "");
+                                    } else {
+                                        System.out.println("没库存了，stock:" + stock);
+                                    }
+                                } finally {
+                                    lock.unlock();
+                                }
+                            }
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+            ).start();
+
         }
     }
 
