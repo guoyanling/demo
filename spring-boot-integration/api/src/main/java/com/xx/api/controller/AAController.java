a3852b23bf8b5dfac30f294fdd8d382c6c98e239
Kealina <kealina@qq.com>
2019-09-10 22:51:19 +0800

    igitignore

---
+++ spring-boot-integration/api/src/main/java/com/xx/api/controller/AAController.java
@@ -0,0 +1,13 @@
+package com.xx.api.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AAController {
+
+    @GetMapping
+    public void a() {
+
+    }
+}
